fefe9f1c61fe7e01308f9c050910cad9724243ea
ilhom temitrov <ilhomtemitrov@ilhoms-MacBook-Pro.local>
2021-02-17 02:08:46 +0900

    feat: ortacha js bilan harakat

--- harakat.js
+++ harakat.js
@@ -1,7 +1,10 @@
 let tugma = document.querySelector("#butt1")
 tugma.addEventListener("click", function() {
-    let ism = document.querySelector("#soz").value
-    alert('salom hurmatli: ' + ism)
+    let sahifa = document.querySelector("#bosh")
+    // sahifa.classList.remove('bizniki')
+    sahifa.classList.add('sizniki')
+    // let ism = document.querySelector("#soz").value
+    // alert('salom hurmatli: ' + ism)
 })
 
 function salomber() {
