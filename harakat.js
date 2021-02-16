6e02721b3a6c839d0ee322c437b5cd103bb7e3d5
ilhom temitrov <ilhomtemitrov@ilhoms-MacBook-Pro.local>
2021-02-17 02:00:04 +0900

    feat: js kiritilgan text chiqarish

--- harakat.js
+++ harakat.js
@@ -1,6 +1,7 @@
 let tugma = document.querySelector("#butt1")
 tugma.addEventListener("click", function() {
-    alert('check')
+    let ism = document.querySelector("#soz").value
+    alert('salom hurmatli: ' + ism)
 })
 
 function salomber() {
